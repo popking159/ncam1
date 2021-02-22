2c11a9ff90b9b1d0a08f377a74ebdf4b86e2e8a2
popking159 <47087407+popking159@users.noreply.github.com>
2021-02-23 01:37:21 +0200

    Add files via upload

---
+++ cardlist.h
@@ -0,0 +1,9 @@
+#ifndef MODULE_CARDLIST_H_
+#define MODULE_CARDLIST_H_
+
+#ifdef WITH_CARDLIST
+struct atrlist { char providername[32]; char atr[80]; char info[92]; };
+void findatr(struct s_reader *reader);
+#endif // WITH_CARDLIST
+
+#endif
